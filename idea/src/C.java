2b154ed93976832431188d5305456d3b2070a046
chenweipeng1993 <1091236678@qq.com>
2018-04-19 23:20:16 +0800

    idea的快捷

--- idea/src/C.java
+++ idea/src/C.java
@@ -1,3 +1,9 @@
 public class C {
-
+    public static void main(String[] args){
+        StringBuilder str = new StringBuilder("aaa");
+        StringBuilder str2 = str.append("bbb");
+        System.out.println(str2);
+        StringBuilder str3 = str.append("ccc");
+        System.out.println(str3);
+    }
 }
--- idea/src/Status.java
+++ idea/src/Status.java
@@ -1,11 +1,11 @@
 public class Status {
-    200: "OK"
+    /*200: "OK"
             201: "Created"
             202: "Accepted"
             203: "Non-Authoritative Information"
             204: "No Content"
             205: "Reset Content"
-            206: "Partial Content"
+            206: "Partial Content"*/
     private int code;
 
     public Status(int code) {
